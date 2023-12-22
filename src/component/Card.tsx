e1fc015393b94dc0acca56d389cbfdea86d1d367
Jason Raymond Reyes <jasonxxix@gmail.com>
2023-12-22 16:58:31 +0800

    Create Card for About and Projects.

---
+++ src/component/Card.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+interface CardType {
+  src: string;
+  alt: string;
+  textarea: string;
+} 
+
+export default function Card({
+  src,
+  alt,
+  textarea,
+}: CardType) {
+    return (
+      <div className="card_container text-center">
+        <Image src={src} alt={alt} width={200} height={200} />
+        <b>{textarea}</b>
+      </div>
+    )
+  }
