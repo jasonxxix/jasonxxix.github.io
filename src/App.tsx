07f58dcd5ae6374390fd1544f56322586791e878
Jason Raymond Reyes <jasonxxix@gmail.com>
2023-12-28 18:41:13 +0800

    Applied Card of About and Projects from NextJs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from './component/Card'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card src="/images/person.jpg" alt="Person" textarea={"About"} />
+      <Card src="/images/projects.jpg" alt="QuestionMark" textarea={"Projects"} />
     </div>
   );
 }
---
+++ src/component/Card.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+interface CardType {
+  src: string;
+  alt: string;
+  textarea: string;
+} 
+
+export default function Card({
+  src,
+  alt,
+  textarea,
+}: CardType) {
+    return (
+      <CardDiv>
+        <img src={src} alt={alt} width={200} height={200} />
+        <b>{textarea}</b>
+      </CardDiv>
+    )
+  }
+
+const CardDiv = styled.div`
+  height: 600px;
+  width: 300px;
+  background-color: #c2c2d6;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  margin: 20px;
+  transition: transform 1s;
+
+  img{
+    border-radius: 100%;
+    border-color: #d1d1e0;
+    border-width: 2px;
+    margin-bottom: 10px;
+  };
+
+  b{
+    font-size: 42px;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    color: var(--main-bg-color);
+  };
+
+  &:hover{
+    transform: rotateY(360deg);
+    cursor: pointer;
+  };
+`
