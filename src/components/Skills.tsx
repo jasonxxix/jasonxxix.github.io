b455fcc71c25235bb14203d28d1edea7018ea6a3
Jason Raymond Reyes <jasonxxix@gmail.com>
2024-01-02 18:46:39 +0800

    Fix Sql to SQL.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -12,7 +12,7 @@ export default function Skills() {
             <CardItem title='Mobile Development' skillList={['Android Java','NDK','Gradle','Espresso']}/>
             <CardItem title='AWS Services' skillList={['Lambda', 'API Gateway', 'S3 Bucket', 'Cloud Search', ]} />
             <CardItem title='Version Control' skillList={['GitHub', 'Jira', 'Code Review', 'Documentation']}/>
-            <CardItem title='Database' skillList={['MongoDb', 'MySql', 'Redshift', 'MicrosofstSql', 'PostgresSql']} />
+            <CardItem title='Database' skillList={['MongoDb', 'MySQL', 'Redshift', 'MicrosofstSQL', 'PostgresSQL']} />
             <CardItem title='Programming Language' skillList={['HTML','CSS','Python','Java','C++', 'R']}/>
         </div>
     </div>
