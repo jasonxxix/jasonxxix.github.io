1bd7a5b97a145dc348855f1cd3c3c0c3de218f5c
Jason Raymond Reyes <jasonxxix@gmail.com>
2024-01-02 18:35:39 +0800

    Updated AboutMe Content.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,10 +1,10 @@
 
 export default function AboutMe() {
     return <div className="py-4 px-6 max-w-prose m-auto">
-        <h1 className="text-2xl">About Me</h1>
-        <p className="text-lg border-t-2 border-emerald-400 px-2 text-justify">Lorem ipsum dolor sit amet.<br />
-        Rem dolorum totam et nemo facilis et cumque galisum.<br />
-        Qui dolor accusantium ex minima eius aut debitis accusantium non temporibus laudantium non reiciendis ipsa ut dignissimos nesciunt.<br />
-        Qui quam ratione et saepe magnam et rerum incidunt qui fugiat molestiae rem saepe quae.</p>
+        <h1 className="text-2xl  border-b-2 border-emerald-400">About Me</h1>
+        <p className="text-lg px-2">Hi Jason Reyes here,<br />
+        I'm currently residing in Quezon City, Philippines and employed as Research and Developer Specialist in GMA New Media Inc. This has been my home for 6 years already.<br /><br />
+        Besides from Programming and learning new techonlogies, I spend my time traveling, exploring foods, gaming, and investing.<br /><br />
+        I believe that each one of us need to have natural cuoriosity as this triggers hunger for people to learn.</p>
     </div>
 }
