6132ad255fb8b24f89d14daafc5393bdbb31b7d4
Jason Raymond Reyes <jasonxxix@gmail.com>
2024-01-02 15:35:57 +0800

    Adjusted to Mobile First Approach.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,26 @@ import useStyleMode from "../hooks/useStyleMode";
 
 function generateItem (text: string, index: number) {
     return (<div key={index}>
-        <Separator orientation="vertical" />
-        <div>{text}</div>
+        <li>{text}</li>
     </div>)
+}
+
+const burgerShow = () => {
+    const burgerOpen = document.getElementById("burger-open");
+    burgerOpen?.classList.replace("block", "hidden");
+    const burgerClose = document.getElementById("burger-close");
+    burgerClose?.classList.replace("hidden", "block");
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu?.classList.replace("hidden", "block");
+}
+
+const burgerHide = () => {
+    const burgerOpen = document.getElementById("burger-open");
+    burgerOpen?.classList.replace("hidden", "block");
+    const burgerClose = document.getElementById("burger-close");
+    burgerClose?.classList.replace("block", "hidden");
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu?.classList.replace("block", "hidden");
 }
 
 export default function Header(props: { tabs: string[], user: User }) {
@@ -17,21 +34,38 @@ export default function Header(props: { tabs: string[], user: User }) {
     const { textDarkMode, toggleDarkMode} = useStyleMode();
     return (
         <header className="border-slate-200 border-b p-3 flex justify-between bg-white dark:bg-slate-600">
-            <div className="flex shrink space-x-2">
-                <div className="pl-2">Portfolio: </div>
-                <Avatar src={user.src} alt={generateAlt(user.name, "avatar")} className="max-h-6"/>
-                <div>{user.name}</div>
-                <Separator orientation="vertical" />
+            <div className="flex flex-col md:flex-row shrink md:space-x-2 truncate">
+                <div className="inline-flex"><Avatar src={user.src} alt={generateAlt(user.name, "avatar")} className="max-h-6 max-w-6 mr-1"/>{user.name}</div>
+                <Separator className="hidden md:block" orientation="vertical" />
                 <div>{user.position}</div>
             </div>
-            <div className="flex h-5 items-center space-x-4 justify-end">
+            <ul className="flex flex-col md:hidden">
+                <div className="flex justify-end">
+                    <svg id="burger-open" className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true"
+                        onClick={burgerShow}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                    <svg id="burger-close" className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true"
+                        onClick={burgerHide}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    
+                </div>
+                <div className="hidden md:hidden" id="mobile-menu">
+                    <div className="space-y-1 px-2 pb-3 pt-2">
+                        {tabs.map(generateItem)}
+                    </div>
+                </div>
+            </ul>
+          
+            <ul className="hidden md:flex h-5 items-center space-x-4 justify-end">
                 {tabs.map(generateItem)}
                 <Separator orientation="vertical" />
                 <Switch 
                     onCheckedChange={toggleDarkMode}
                 />
                 <div className="w-24">{textDarkMode}</div>
-            </div>
+            </ul>
         </header>
     )
 }
